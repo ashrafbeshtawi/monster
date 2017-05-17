99757b359f4065cf70e620cc7217d4ce4cbb5af9
Ashraf <beshtawi.ashraf@gmail.com>
2017-05-17 20:31:39 +0200

    up
    up

---
+++ print.js
@@ -0,0 +1,34 @@
+function monsters(){ 
+var destiny1 = prompt("The moster will give u 3 choises RUN , FIGHT , PAY ","Type here your dessiccion").toUpperCase();
+switch (destiny1){
+    case "RUN" :
+   var destiny2 = prompt("Can u run faster than 8 Km/hour ?" , "Yes or No").toUpperCase();
+    if (destiny2==="YES") {
+     document.write("<p class=contex> u survived </p>");
+     }else {
+      document.write("<p class=contex> u r dead </p>");  
+    }
+    break ;
+        case "FIGHT" :
+   var destiny2 = prompt("R U strong ?" , "Yes or No").toUpperCase();
+    if (destiny2==="YES") {
+     document.write("<p class=contex> u could beat the monster </p>");
+     }else {
+      document.write("<p class=contex> u should not do this.. u r dead dude </p>");   
+    }
+    break ;
+         case "PAY" :
+   var destiny2 = prompt("Di u have 100$ ?" , "Yes or No").toUpperCase();
+    if (destiny2==="YES") {
+     document.write("<p class=contex>Thanks for the money i will eat u anyway </p>");
+     }else {
+      document.write("<p class=contex> Sorry dude u have to pay </p>");   
+    }
+    break ;
+    default :
+    document.write("<p class=contex> I dont understand u dude </p>")
+    break;
+}
+}
+
+document.getElementById("monsters").onclick =   monsters;
